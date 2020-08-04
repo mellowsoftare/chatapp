bcfe3286d38e32ccbc64bd5ff672d58986d63263
Brandon <bmccarthy@mellowmedia.net>
2020-08-04 16:15:03 -0500

    first commit

---
+++ client/src/components/Messages/Message/Message.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './Message.css';
+
+import ReactEmoji from 'react-emoji';
+
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+  const trimmedUser = user.trim().toLowerCase();
+
+  if(user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+
+  if(trimmedUser === 'admin'){
+    return(
+      <div className="messageContainer justifyStart">
+        <div className="messageBoxAdmin">
+          <p className="messageText colorLightDark"><em>{ReactEmoji.emojify(text)}</em></p>
+        </div>
+        <p className="sentText pl-10 ">{user}</p>
+      </div>
+    )
+  }else if(isSentByCurrentUser === true){
+    return (
+      <div className="messageContainer justifyEnd">
+        <p className="sentText pr-10">{trimmedName}</p>
+        <div className="messageBox backgroundBlue">
+          <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+        </div>
+      </div>
+    )
+  }else{
+    return(
+      <div className="messageContainer justifyStart">
+        <div className="messageBox backgroundLight">
+          <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+        </div>
+        <p className="sentText pl-10 ">{user}</p>
+      </div>
+    )
+  }
+
+}
+
+export default Message;
